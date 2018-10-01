e3d5e6eb92eff3005d91cf49d856e8c9a9feb044
aldrinmagno <aldrinmagno@gmail.com>
2018-10-01 20:30:54 +0930

    added models, initial migration

--- AnyoneTennis/AnyoneTennis/Startup.cs
+++ AnyoneTennis/AnyoneTennis/Startup.cs
@@ -1,5 +1,20 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnyoneTennis.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.HttpsPolicy;
+// using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
 
 [assembly: OwinStartupAttribute(typeof(AnyoneTennis.Startup))]
 namespace AnyoneTennis
@@ -10,5 +25,24 @@ namespace AnyoneTennis
         {
             ConfigureAuth(app);
         }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //  var connection = @"Server=(ehsesql1prd.cdu.edu.au)\mssqllocaldb;Database=tennis;Trusted_Connection=True;ConnectRetryCount=0";
+            var connection = @"Server=ehsesql1prd.cdu.edu.au;Database=tennis;User Id=test;Password=easypassword";
+            services.AddDbContext<tennisContext>(options => options.UseSqlServer(connection));
+        }
+
     }
 }
