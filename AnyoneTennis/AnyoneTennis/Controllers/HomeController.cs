a36456bb3e8e656364b77aa8da8ac520ff64228f
guzzitt <augustine.t@icloud.com>
2018-10-13 14:27:11 +0930

    changes setup

--- AnyoneTennis/AnyoneTennis/Controllers/HomeController.cs
+++ AnyoneTennis/AnyoneTennis/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnyoneTennis.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,17 @@ namespace AnyoneTennis.Controllers
 {
     public class HomeController : Controller
     {
+
+      /*  private readonly tennisContext _db;
+
+        public HomeController(tennisContext db)
+        {
+            _db = db;
+        } */
+
         public ActionResult Index()
         {
+     //       var coach = _db.Coach.OrderByDescending(x => x.CoachId).Take(5).ToArray();
             return View();
         }
 
--- AnyoneTennis/AnyoneTennis/Startup.cs
+++ AnyoneTennis/AnyoneTennis/Startup.cs
@@ -1,17 +1,8 @@
 ﻿using Microsoft.Owin;
 using Owin;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 using AnyoneTennis.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-=======
->>>>>>> parent of e3d5e6e... added models, initial migration
-=======
->>>>>>> parent of e3d5e6e... added models, initial migration
-=======
->>>>>>> parent of e3d5e6e... added models, initial migration
 
 [assembly: OwinStartupAttribute(typeof(AnyoneTennis.Startup))]
 namespace AnyoneTennis
@@ -22,9 +13,6 @@ namespace AnyoneTennis
         {
             ConfigureAuth(app);
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,11 +21,5 @@ namespace AnyoneTennis
             var connection = @"Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True;ConnectRetryCount=0";
             services.AddDbContext<tennisContext>(options => options.UseSqlServer(connection));
         }
-=======
->>>>>>> parent of e3d5e6e... added models, initial migration
-=======
->>>>>>> parent of e3d5e6e... added models, initial migration
-=======
->>>>>>> parent of e3d5e6e... added models, initial migration
     }
 }
